bc69bf082b20d5188be8ae11befc1aac73af56f7
mstf-gdik <mstf.gdik19@gmail.com>
2023-01-30 15:44:21 +0300

    Redis Complex Types Caching Json

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 
 namespace IDistributedCacheRedisApp.Web.Controllers;
 
@@ -14,22 +16,27 @@ public class ProductsController : Controller
 
     public async Task<IActionResult> Index()
     {
-        await _distributedCache.SetStringAsync("name", "Mustafa", new DistributedCacheEntryOptions
+        var product = new Product { Id = 1, Name = "Kalem", Price = 185.00M };
+        var productSerialize = JsonSerializer.Serialize(product);
+
+        await _distributedCache.SetStringAsync("product:1", productSerialize, new DistributedCacheEntryOptions
         {
             AbsoluteExpiration = DateTime.Now.AddMinutes(1)
         });
+
         return View();
     }
 
     public async Task<IActionResult> Show()
     {
-        ViewBag.name = await _distributedCache.GetStringAsync("name");
+        var product = await _distributedCache.GetStringAsync("product:1");
+        ViewBag.product = JsonSerializer.Deserialize<Product>(product);
         return View();
     }
 
     public async Task<IActionResult> Remove()
     {
-        await _distributedCache.RemoveAsync("name");
+        await _distributedCache.RemoveAsync("product:1");
         return View();
     }
 }
