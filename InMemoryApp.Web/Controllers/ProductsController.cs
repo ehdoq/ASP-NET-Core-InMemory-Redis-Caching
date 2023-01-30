7be39f75c1e51f9f7d44de95588ba94238abb3aa
mstf-gdik <mstf.gdik19@gmail.com>
2023-01-30 11:37:48 +0300

    InMemory Get() Set()

---
+++ InMemoryApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers;
+
+public class ProductsController : Controller
+{
+    private readonly IMemoryCache _memoryCache;
+
+    public ProductsController(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+    public IActionResult Index()
+    {
+        _memoryCache.Set("zaman", DateTime.Now.ToString());
+        return View();
+    }
+
+    public IActionResult Show()
+    {
+        ViewBag.zaman = _memoryCache.Get("zaman");
+        return View();
+    }
+}
