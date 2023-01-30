a98539f85422f408198f46ecc98cb5fb924ec8d5
mstf-gdik <mstf.gdik19@gmail.com>
2023-01-30 11:54:22 +0300

    InMemory TryGetValue(), Remove(), GetOrCreate()

--- InMemoryApp.Web/Controllers/ProductsController.cs
+++ InMemoryApp.Web/Controllers/ProductsController.cs
@@ -14,13 +14,28 @@ public class ProductsController : Controller
 
     public IActionResult Index()
     {
-        _memoryCache.Set("zaman", DateTime.Now.ToString());
+        //1.Yol = Key'in memory'de olup olmadığıyla ilgili kontrol. 
+        //if (string.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+        //    _memoryCache.Set("zaman", DateTime.Now.ToString());
+        
+        //2.Yol
+        if(!_memoryCache.TryGetValue("zaman", out string zamanCache))
+            _memoryCache.Set("zaman", DateTime.Now.ToString());
+
+        ViewBag.zaman = zamanCache;
+
         return View();
     }
 
     public IActionResult Show()
     {
-        ViewBag.zaman = _memoryCache.Get("zaman");
+        ViewBag.zaman = _memoryCache.GetOrCreate("zaman", entry =>
+        {
+            return DateTime.Now.ToString();
+        });
+
+        _memoryCache.Remove("zaman");
+
         return View();
     }
 }
