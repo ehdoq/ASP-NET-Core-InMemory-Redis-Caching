61ee9e9c632504e534738e64f8732b5bbbac01aa
mstf-gdik <mstf.gdik19@gmail.com>
2023-01-31 09:00:44 +0300

    StackExchange.Redis 3

---
+++ RedisExchange.Api/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchange.Api.Models;
+using RedisExchange.Api.Repositories;
+
+namespace RedisExchange.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductsController(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await _productRepository.GetAllAsync());
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        return Ok(await _productRepository.GetByIdAsync(id));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Product product)
+    {
+        return Created(string.Empty, await _productRepository.CreateAsync(product));
+    }
+}
--- RedisExchange.Api/Program.cs
+++ RedisExchange.Api/Program.cs
@@ -1,13 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using RedisExchange.Api.Models;
+using RedisExchange.Api.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("MyDatabase");
+});
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 var app = builder.Build();
+
+using (var scoped = app.Services.CreateScope())
+{
+    var dbContext = scoped.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
