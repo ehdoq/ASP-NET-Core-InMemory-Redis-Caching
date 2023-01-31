536f022fc22af86bd2afa746ccb8702a90f51835
mstf-gdik <mstf.gdik19@gmail.com>
2023-01-31 09:00:09 +0300

    StackExchange.Redis 2

---
+++ RedisExchange.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using RedisExchange.Api.Models;
+
+namespace RedisExchange.Api.Repositories;
+
+public interface IProductRepository
+{
+    Task<List<Product>> GetAllAsync();
+    Task<Product> GetByIdAsync(int id);
+    Task<Product> CreateAsync(Product product);
+}
---
+++ RedisExchange.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedisExchange.Api.Models;
+
+namespace RedisExchange.Api.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public ProductRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task<List<Product>> GetAllAsync()
+    {
+        return await _appDbContext.Products.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Product> GetByIdAsync(int id)
+    {
+        return await _appDbContext.Products.FindAsync(id);
+    }
+
+    public async Task<Product> CreateAsync(Product product)
+    {
+        await _appDbContext.Products.AddAsync(product);
+        await _appDbContext.SaveChangesAsync();
+
+        return product;
+    }    
+}
